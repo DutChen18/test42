5045350925d9f0d40afb9fa0232db83b1c582121
Chen Steenvoorden <csteenvo@student.codam.nl>
2021-11-22 14:20:53 +0100

    getting somewhere

--- main.c
+++ main.c
@@ -1,4 +1,3 @@
-#include "test.h"
 #include <dlfcn.h>
 #include <stdlib.h>
 
@@ -6,12 +5,16 @@ int
 	main(int argc, char **argv)
 {
 	void	*dl;
+	long	*fail_malloc;
+	int		*do_test_mem;
 
-	if (argc > 3)
-		fail_malloc = atoi(argv[3]);
-	if (argc > 4)
-		do_test_mem = atoi(argv[4]);
 	dl = dlopen(argv[1], RTLD_LAZY);
+	fail_malloc = dlsym(dl, "fail_malloc");
+	do_test_mem = dlsym(dl, "do_test_mem");
+	if (argc > 3)
+		*fail_malloc = atoi(argv[3]);
+	if (argc > 4)
+		*do_test_mem = atoi(argv[4]);
 	((void (*)(void)) dlsym(dl, argv[2]))();
 	return (EXIT_SUCCESS);
 }
--- test.py
+++ test.py
@@ -1,7 +1,7 @@
 import asyncio
 import sys
 
-proc_sem = asyncio.Semaphore(10)
+proc_sem = None
 tests = []
 
 async def pexec(cmd, stdin=b""):
@@ -21,6 +21,12 @@ class Exec:
 	async def __await__(self):
 		self.result = await pexec(self.cmd)
 
+class Case(Exec):
+	def __init__(self, cmd, opt=False):
+		Exec.__init__(self, cmd)
+		self.opt = opt
+		self.ok = False
+
 class Test:
 	def __init__(self, name):
 		self.execs = []
@@ -33,15 +39,53 @@ class Test:
 		await asyncio.gather(*self.cases.values())
 		for name, case in self.cases.items():
 			stdout, stderr, returncode = case.result
-			
-			print(stdout, stderr, returncode)
+			result = b""
+			result += f"stdout {len(stdout)}\n".encode() + stdout
+			result += f"stderr {len(stderr)}\n".encode() + stderr
+			path = f"{project_name}/{self.name}/{name}"
+			if mode == "test":
+				diff = await pexec(["diff", path + ".txt", "-"], result)
+				with open(path + ".diff", "wb") as f:
+					f.write(diff[0])
+				case.ok = diff[0] == b""
+			elif mode == "record":
+				with open(path + ".txt", "wb") as f:
+					f.write(result)
+
+		result_str = ""
+		result_arr = ""
+		for name, case in self.cases.items():
+			if case.ok:
+				result_str += " \033[1;32mOK\033[0m "
+			elif case.opt:
+				result_str += " \033[1;33mKO\033[0m "
+				result_arr += f"\n\033[1;33mKO\033[0m \033[1;37m{name}\033[0m"
+			else:
+				result_str += " \033[1;31mKO\033[0m "
+				result_arr += f"\n\033[1;31mKO\033[0m \033[1;37m{name}\033[0m"
+		print(f"\033[1;37m{self.name:16}\033[0m{result_str}{result_arr}")
+
+class CTest(Test):
+	def __init__(self, name, args):
+		Test.__init__(self, name)
+		self.execs.append(Exec(["cc", *args, "-shared", "test.c",
+			f"{project_name}/{self.name}/{self.name}.c",
+			"-o", f"{project_name}/{self.name}/{self.name}.so"]))
+
+	def add(self, name):
+		self.cases[name] = Case(["./test",
+			f"{project_name}/{self.name}/{self.name}.so",
+			f"main_{name}"])
 
 async def main(module, argv):
-	await pexec(["cc", "test.c", "-o", "test"])
+	global proc_sem
+	proc_sem = asyncio.Semaphore(4)
+	await pexec(["cc", "main.c", "-o", "test"])
 	module.main(sys.modules[__name__], argv)
 	await asyncio.gather(*[test.run() for test in tests])
 
 if __name__ == "__main__":
 	mode = sys.argv[1]
-	module = __import__(sys.argv[2])
+	project_name = sys.argv[2]
+	module = __import__(project_name)
 	asyncio.run(main(module, sys.argv[3:]))
